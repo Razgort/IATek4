28f7d15e625e2749735c32f279160ed28cb326be
Jon <jon@jon>
2015-10-22 11:40:16 +0200

    "Update"

--- step1/ocr.py
+++ step1/ocr.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 
 import pybrain
+import Image as im
+import numpy as np
+
+def getImageArray(imagepath):
+    try:
+      image = im.open(imagepath)
+      imagearray=np.asarray(image.crop(image.getbbox()).resize((10,10))).astype(float)
+      return imagearray
+    except IOError:
+      print "File Not Found"
+      return np.zeros((10,10))
+
 
 def main():
   print("Starting Neural Net")
+  print(getImageArray("data/a.bmp"))
 
 if __name__ == "__main__":
   main()
