4f9ff0e9167666ad68ed8f485f4083abdae7e085
razgort <alexandre.moulay@epitech.eu>
2015-10-23 13:46:45 +0200

    First

--- step1/ocr.py
+++ step1/ocr.py
@@ -1,34 +1,45 @@
 #!/usr/bin/python
 
-import utils 
+import utils
 from pybrain.structure import FeedForwardNetwork
 from pybrain.structure import LinearLayer, SigmoidLayer
 from pybrain.structure import FullConnection
 
+
 class nn:
-  def __init__(self, input_path, output_path):
-    self.n = FeedForwardNetwork()
-    self.pix_size = 50
-    self.input_value = utils.readLines(input_path)
-    self.output_value = utils.readLines(output_path)
-    self.inputUnits = self.pix_size * self.pix_size 
-    self.nbHiddenLayers = 1
-    self.hiddenUnits = 500
-    self.outputUnits = 1
 
-    inputLayer = LinearLayer(self.inputUnits)
-    hiddenLayer = SigmoidLayer(self.hiddenUnits)
-    outputLayer = LinearLayer(self.outputUnits)
-  
-    input_to_hidden = FullConnection(inputLayer, hiddenLayer)
-    hidden_to_output = FullConnection(hiddenLayer, outputLayer) 
+    def __init__(self, input_path, output_path):
+        self.n = FeedForwardNetwork()
+        self.pix_size = 50
+        self.input_value = utils.readLines(input_path)
+        self.output_value = utils.readLines(output_path)
+        self.inputUnits = self.pix_size * self.pix_size
+        self.nbHiddenLayers = 1
+        self.hiddenUnits = 500
+        self.outputUnits = len(self.output_value)
 
-    self.n.addConnection(input_to_hidden)
-    self.n.addConnection(hidden_to_output)
+        inputLayer = LinearLayer(self.inputUnits)
+        hiddenLayers = []
+        for i in range(0, self.nbHiddenLayers):
+            hiddenLayers.append(SigmoidLayer(self.hiddenUnits))
+        outputLayer = LinearLayer(self.outputUnits)
+
+        input_to_hidden = FullConnection(inputLayer, hiddenLayers[0])
+        hidden_to_hidden = []
+        if len(hiddenLayers) > 1:
+            for i in range(1, len(hiddenLayers) - 2):
+                hidden_to_hidden.append(
+                    FullConnection(hiddenLayers[i], hiddenLayers[i + 1]))
+        hidden_to_output = FullConnection(
+            hiddenLayers[len(hiddenLayers) - 1], outputLayer)
+        self.n.addConnection(input_to_hidden)
+        for hidden in hidden_to_hidden:
+            self.n.addConnection(hidden)
+        self.n.addConnection(hidden_to_output)
 
 
 def main():
-  nn("./input_data", "./output_data")
+    nn("./input_data", "./output_data")
 
 if __name__ == "__main__":
-  main()
+    main()
--- step1/utils.py
+++ step1/utils.py
@@ -1,26 +1,28 @@
 import numpy as np
 import Image as im
 
+
 def getImageArray(imagepath, units):
-  try:
-    image = im.open(imagepath)
-    imagearray = np.asarray(image.crop(image.getbbox()).resize((units, units)))
-    return imagearray
-  except IOError:
-    print("File not found !")
-    exit()
-    return np.zeros((10,10))
+    try:
+        image = im.open(imagepath)
+        imagearray = np.asarray(
+            image.crop(image.getbbox()).resize((units, units)))
+        return imagearray
+    except IOError:
+        print("File not found !")
+        exit()
+        return np.zeros((10, 10))
 
 
 def readLines(path):
-  arr = []
-  try:
-    f = open(path, 'r')
-    for l in f:
-      arr.append(l)
+    arr = []
+    try:
+        f = open(path, 'r')
+        for l in f:
+            arr.append(l)
 
-    f.close()
-  except IOError:
-    print("File not found !")
-    exit()
-  return arr
+        f.close()
+    except IOError:
+        print("File not found !")
+        exit()
+    return arr
